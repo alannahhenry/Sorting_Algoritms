32eb069d241b4cb1ee060bf4d1457606574ad346
alannahhenry <60479099+alannahhenry@users.noreply.github.com>
2020-02-25 17:44:34 +0000

    Add files via upload

--- src/SortComparison.java
+++ src/SortComparison.java
@@ -18,18 +18,18 @@ class SortComparison {
      *
      */
     static double [] insertionSort (double a[]){
-        double tmp;
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j > 0; j++) {
-                if(a[j]<a[j-1]){
-                    tmp = a[j];
-                    a[j]=a[j-1];
-                    a[j-1]=tmp;
-                }
+        for (int i = 1; i <a.length ; ++i) {
+            double val = a[i];
+            int j = i-1;
+            while((j>=0)&&(a[j]>val)){
+                a[j+1] = a[j];
+                j = j-1;
             }
+            a[j+1] = val;
         }
         return a;
     }
+
     /**
      * Sorts an array of doubles using Selection Sort.
      * This method is static, thus it can be called as SortComparison.sort(a)
@@ -38,16 +38,15 @@ class SortComparison {
      *
      */
     static double [] selectionSort (double a[]){
-        for (int i = 0; i < a.length-1; i++) {
-            int min = i;
-            for (int j = i+1; j < a.length ; j++) {
-                if(a[j]<a[min]){
-                    min = j;
-                }
-                double tmp = a[min];
-                a[min]=a[i];
-                a[i] = tmp;
+        for (int i = 0; i <a.length ; i++) {
+            int c = i;
+            for (int j = i+1; j <a.length ; j++) {
+                if(a[j] < a[c])
+                    c=j;
             }
+            double smallNum = a[c];
+            a[c] = a[i];
+            a[i] = smallNum;
         }
         return a;
     }
@@ -60,10 +59,32 @@ class SortComparison {
      *
      */
     static double [] quickSort (double a[]){
-
-        //todo: implement the sort
-
-    }//end quicksort
+        recursiveQSort(a, 0, a.length-1);
+        return a;
+    }
+    private static void recursiveQSort(double[] a, int lo, int hi){
+        if(hi<=lo)
+            return;
+        int pivot = partition(a, lo, hi);
+        recursiveQSort(a, lo, pivot-1);
+        recursiveQSort(a, pivot+1, hi);
+    }
+    private static int partition (double a[], int lo, int hi){
+        double pivot = a[hi];
+        int i = (lo-1);
+        for(int j = lo; j<hi;j++){
+            if(a[j]< pivot) {
+                i++;
+                double tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+        }
+        double tmp = a[i+1];
+        a[i+1] = a[hi];
+        a[hi] = tmp;
+        return i+1;
+    }
 
     /**
      * Sorts an array of doubles using Merge Sort.
@@ -81,35 +102,128 @@ class SortComparison {
      */
 
     static double[] mergeSortIterative (double a[]) {
+        int current_size;
+        int left_start;
 
-        //todo: implement the sort
+        for(current_size=1; current_size <= a.length-1; current_size = 2*current_size){
+            for (left_start=0; left_start < a.length-1;left_start+=2*current_size){
+                int mid = Math.min(left_start+current_size-1, a.length-1);
+                int right_end = Math.min(left_start + 2*current_size -1, a.length-1);
+                merge(a, left_start, mid, right_end);
+            }
+        }
+        return a;
 
-    }//end mergesortIterative
-    static int partition_quicksort(double a[], int lo, int hi){
-        double pivot = a[hi];
-        return 0;
     }
+    private static void merge(double a[], int left, int mid, int right){
+        int i, j, k;
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        double L[] = new double[n1];
+        double R[] = new double[n2];
+        for ( i= 0; i <n1 ; i++) {
+            L[i] = a[left+i];
+        }
+        for (j=0;j<n2;j++){
+            R[j] = a[mid+1+j];
+        }
+        i = 0;
+        j = 0;
+        k = left;
+        while(i<n1 && j<n2){
+            if(L[i]<=R[j]){
+                a[k] =L[i];
+                i++;
+            }
+            else{
+                a[k] =R[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<n1){
+            a[k] = L[i];
+            i++;
+            k++;
+        }
+        while(j<n1){
+            a[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
 
 
     /**
      * Sorts an array of doubles using recursive implementation of Merge Sort.
      * This method is static, thus it can be called as SortComparison.sort(a)
      *
-     * @param a: An unsorted array of doubles.
+     * @param array: An unsorted array of doubles.
      * @return after the method returns, the array must be in ascending sorted order.
      */
-    static double[] mergeSortRecursive (double a[]) {
+    static double[] mergeSortRecursive (double array[]) {
+        if(array == null)
+        {
+            return array;
+        }
 
+        if(array.length > 1)
+        {
+            int mid = array.length / 2;
 
-        //todo: implement the sort
+            // Split left part
+            double[] left = new double[mid];
+            for(int i = 0; i < mid; i++)
+            {
+                left[i] = array[i];
+            }
 
-    }//end mergeSortRecursive
+            // Split right part
+            double[] right = new double[array.length - mid];
+            for(int i = mid; i < array.length; i++)
+            {
+                right[i - mid] = array[i];
+            }
+            mergeSortRecursive(left);
+            mergeSortRecursive(right);
 
+            int i = 0;
+            int j = 0;
+            int k = 0;
 
-
-
-
-
+            // Merge left and right arrays
+            while(i < left.length && j < right.length)
+            {
+                if(left[i] < right[j])
+                {
+                    array[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+            // Collect remaining elements
+            while(i < left.length)
+            {
+                array[k] = left[i];
+                i++;
+                k++;
+            }
+            while(j < right.length)
+            {
+                array[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+        return array;
+    }
 
     public static void main(String[] args) {
 
--- src/SortComparisonTest.java
+++ src/SortComparisonTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.Arrays;
+
 //-------------------------------------------------------------------------
 /**
  *  Test class for SortComparison.java
@@ -28,19 +30,41 @@ public class SortComparisonTest
      * Check that the methods work for empty arrays
      */
     @Test
-    public void testEmpty()
-    {
+    public void testEmpty(){
+        double[] array = new double[10];
+        assertEquals("Checking insertion sort empty array", "[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]", Arrays.toString(SortComparison.insertionSort(array)));
+        assertEquals("Checking selection sort empty array", "[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]", Arrays.toString(SortComparison.selectionSort(array)));
+        assertEquals("Checking quick sort empty array", "[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]", Arrays.toString(SortComparison.quickSort(array)));
+        assertEquals("Checking mergeSortIterative empty array", "[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]", Arrays.toString(SortComparison.mergeSortRecursive(array)));
+        assertEquals("Checking mergeSortRecursive empty array", "[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]", Arrays.toString(SortComparison.mergeSortRecursive(array)));
+    }
+
+    @Test
+    public void testOneElement(){
+        double[] array = new double[1];
+        array[0] = 69;
+        assertEquals("Checking insertion sort one element array", "[69.0]", Arrays.toString(SortComparison.insertionSort(array)));
+        assertEquals("Checking selection sort one element array", "[69.0]", Arrays.toString(SortComparison.selectionSort(array)));
+        assertEquals("Checking quick sort one element array", "[69.0]", Arrays.toString(SortComparison.quickSort(array)));
+        assertEquals("Checking mergeSortIterative one element array", "[69.0]", Arrays.toString(SortComparison.mergeSortRecursive(array)));
+        assertEquals("Checking mergeSortRecursive one element array", "[69.0]", Arrays.toString(SortComparison.mergeSortRecursive(array)));
+    }
+    @Test
+    //16, 21, 24, 25, 33, 50, 68, 79, 92, 99
+    public void testTenElement(){
+        double[] array = new double[]{99.0, 16.0, 25.0, 33.0, 79.0, 50.0, 21.0, 92.0, 24.0, 68.0};
+        assertEquals("Checking insertion sort ten element unsorted array", "[16.0, 21.0, 24.0, 25.0, 33.0, 50.0, 68.0, 79.0, 92.0, 99.0]", Arrays.toString(SortComparison.insertionSort(array)));
+        assertEquals("Checking selection sort ten element unsorted array", "[16.0, 21.0, 24.0, 25.0, 33.0, 50.0, 68.0, 79.0, 92.0, 99.0]", Arrays.toString(SortComparison.selectionSort(array)));
+        assertEquals("Checking quick sort ten element unsorted array", "[16.0, 21.0, 24.0, 25.0, 33.0, 50.0, 68.0, 79.0, 92.0, 99.0]", Arrays.toString(SortComparison.quickSort(array)));
+        assertEquals("Checking mergeSortIterative ten element unsorted array", "[16.0, 21.0, 24.0, 25.0, 33.0, 50.0, 68.0, 79.0, 92.0, 99.0]", Arrays.toString(SortComparison.mergeSortRecursive(array)));
+        assertEquals("Checking mergeSortRecursive ten element unsorted array", "[16.0, 21.0, 24.0, 25.0, 33.0, 50.0, 68.0, 79.0, 92.0, 99.0]", Arrays.toString(SortComparison.mergeSortRecursive(array)));
+
     }
 
 
     // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
     // be executed at least once from at least one test.
-    @Test
-    public void testInsertionSort(){
-        double a[] = {1};
-        assertEquals("Testing insertionSort with array of size 1", "1", a.toString());
 
-    }
     // ----------------------------------------------------------
     /**
      *  Main Method.
